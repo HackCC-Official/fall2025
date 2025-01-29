5cdd81137eb6260423719ef050e1970be1115b32
Evan Ly <78185158+chheangg@users.noreply.github.com>
2025-01-29 15:59:20 -0800

    Revert "consider prefixes for services"

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        id="app"
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
