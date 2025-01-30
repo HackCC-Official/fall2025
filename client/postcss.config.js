11f9d198de3b9de48e9e6e268e61d4f585ba09ba
Evan Ly <78185158+chheangg@users.noreply.github.com>
2025-01-29 16:00:00 -0800

    Revert "Revert 4 development evan"

---
+++ client/postcss.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { join } = require('path');
+module.exports = {
+  plugins: {
+    tailwindcss: {
+      config: join(__dirname, 'tailwind.config.js'),
+    },
+    autoprefixer: {},
+  },
+};
