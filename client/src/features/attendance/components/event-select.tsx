6cf8d7d09410a69a99f248f2601c3982680c3c19
chheangg <balldremz@gmail.com>
2025-02-16 12:57:42 -0800

    add functionality to event-select

--- client/src/features/attendance/components/event-select.tsx
+++ client/src/features/attendance/components/event-select.tsx
@@ -1,18 +1,18 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { EventDTO } from "@/features/event/types/event-dto";
 
-export function EventSelect() {
+export function EventSelect({ events }: { events: EventDTO[] }) {
   return (
     <Select>
       <SelectTrigger className="w-[200px]">
         <SelectValue placeholder="Event date" />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value="2025-02-04">
-          2025-02-04
-        </SelectItem>
-        <SelectItem value="2025-02-11">
-          2025-02-11
-        </SelectItem>
+        {events.map(e => 
+          <SelectItem key={e.id} value={e.date}>
+            {e.date}
+          </SelectItem>
+        )}
       </SelectContent>
     </Select>
   )
--- client/src/pages/panel/attendance/index.tsx
+++ client/src/pages/panel/attendance/index.tsx
@@ -2,14 +2,28 @@ import { AttendanceSearch } from "@/features/attendance/components/attendance-se
 import PanelLayout from "../layout"
 import { EventSelect } from "@/features/attendance/components/event-select"
 import { AttendanceTab } from "@/features/attendance/components/attendance-tab"
+import { getEvents } from "@/features/event/api/event"
+import { EventDTO } from "@/features/event/types/event-dto"
 
-export default function AttendancePage() {
+export const getServerSideProps = (async () => {
+  try {
+    const [events] = await Promise.all([
+      getEvents()
+    ])
+    return { props: { events } }
+  } catch (error) {
+    console.error('Error fetching data during build:', error);
+    return { props: { events: [] } };
+  }
+})
+
+export default function AttendancePage({ events }: { events: EventDTO[] }) {
   return (
     <div className="py-10">
       <h1 className="font-bold text-3xl">Attendance</h1>
       <div className="flex justify-between items-center mt-8">
         <AttendanceSearch />
-        <EventSelect />
+        <EventSelect events={events} />
       </div>
       <AttendanceTab className="mt-4" />
     </div>
