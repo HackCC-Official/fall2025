37e28633d11ed5d58b36622fb6b095eed25b6a38
chheangg <balldremz@gmail.com>
2025-02-22 21:13:58 -0800

    add env var to docker-compose

--- client/src/features/home-page/components/interest.tsx
+++ client/src/features/home-page/components/interest.tsx
@@ -7,6 +7,8 @@ const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
+console.log(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+
 
 export const Interest = () => {
     const [email, setEmail] = useState("");
