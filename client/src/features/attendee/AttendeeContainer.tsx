ec6594b705de677fe120e7d2cd18083ec5f250c0
Gabriel Flores <cybergabriel36@gmail.com>
2025-03-12 21:24:27 -0700

    added minor fixes to clouds

--- client/src/features/attendee/AttendeeContainer.tsx
+++ client/src/features/attendee/AttendeeContainer.tsx
@@ -1,5 +1,7 @@
 //flex 2xl:flex-row flex-col 2xl:justify-around items-center bg-gradient-to-t from-blue-500 via-transparent to-red-500 mx-auto  
 import Image from "next/image";
+import cloud from "../../../public/Dark Purple Cloud Mid.png"
+import cloud2 from "../../../public/Dark Purple Cloud Back2.png"
 import AttendeeCard from "./ui/AttendeeCard"
 export default function AttendeeContainer() {
     return(
@@ -7,7 +9,9 @@ export default function AttendeeContainer() {
         //lg:desktop/laptop breakpoint
         //initally scaled for mobile->tablet->desktop/laptop
         //IN ORDER FOR CONTAINERS TO WORK! NAME HAS TO BE first_full_name and last initial: example = Gabriel F.
-        <div className="bg-deepsky py-20 2xl:py-60 w-full h-auto">
+        <div className="relative bg-deepsky py-20 2xl:py-60 w-full h-auto">
+            <Image src={cloud} className="top-[150px] absolute w-[200px] h-auto" alt="cloud" />
+            <Image src={cloud2} className="top-[200px] right-0 absolute w-[200px] h-auto" alt="cloud" />
             <div className="mx-auto w-full">
                 <div className="flex md:flex-row flex-col items-center">
                     <AttendeeCard/>
--- client/src/features/attendee/ui/AttendeeCard.tsx
+++ client/src/features/attendee/ui/AttendeeCard.tsx
@@ -4,14 +4,14 @@ import yasir from "../../../../public/yasir.png"
 import quote from "../../../../public/Quote.png"
 export default function AttendeeCard() {
     return (
-        <div className="flex md:flex-row flex-col mx-auto my-10 w-[320px] sm:w-[500px] lg:w-[700px] h-auto md:h-[200px]">
+        <div className="z-10 flex md:flex-row flex-col mx-auto my-10 w-[320px] sm:w-[500px] lg:w-[700px] max-w-[90%] h-auto md:h-[200px]">
             <div className="flex flex-col items-center md:px-4 min-w-[100px] sm:min-w-[150px] lg:min-w-[200px] h-[100%]">
                 <Image src={yasir} alt="person" className="w-[100px] sm:w-[150px] md:w-[100px] lg:w-[200px] h-auto" ></Image>
                 <h2 className="font-bold text-white text-center">Yasir</h2>
             </div>
             <div className="px-4 sm:px-5">
                 <Image src={quote} alt="quote" className="w-[50px] h-auto"></Image>
-                <p className="mt-3 text-[0.8rem] text-white sm:text-[1.2rem] md:text-[0.8rem] lg:text-[1.3rem]">My experience was incredible. I found such a great team to collaborate with and we had a blast creating a silly idea that turned into a big opportunity.</p>               
+                <p className="mt-3 text-[0.8rem] text-white sm:text-[1.2rem] md:text-[0.8rem] lg:text-[1.3rem] break-normal">My experience was incredible. I found such a great team to collaborate with and we had a blast creating a silly idea that turned into a big opportunity.</p>               
             </div>
         </div>
     )
