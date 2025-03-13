7ae9cdf237eb8115153576d62f583691720d4244
chheangg <balldremz@gmail.com>
2025-03-12 17:51:19 -0700

    Revert "Merge branch 'development' of https://github.com/HackCC-Official/spring2025 into development"
    This reverts commit 864975445cd3730e071785587bb6c15e06bc45b0, reversing
    changes made to 9892b40d7dc970ac65da58a3bc596fe407cf5f1c.

--- client/src/features/attendance/types/attendance-query-param.dto.ts
+++
@@ -1,6 +0,0 @@
-import { AttendanceStatus } from "./attendance-dto";
-
-export class AttendanceQueryParamDTO {
-  status?: AttendanceStatus;
-  event_id?: string;
-}
--- client/src/features/outreach/types/interested-users.dto.ts
+++
@@ -1,8 +0,0 @@
-export interface InterestedUserDto {
-    email: string;
-    created_at: string;
-}
-
-export interface AddInterestedUser {
-    email: string;
-}
