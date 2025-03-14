e5056f1d95a47a0cd88f587a91615e908c35b9b0
chheangg <balldremz@gmail.com>
2025-03-14 04:44:49 -0700

    finish application-frontned

--- client/src/features/application/components/form-card.tsx
+++ client/src/features/application/components/form-card.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 export function FormCard({ className, children } : { className?: string, children: React.ReactNode }) {
   return (
     <div className={cn([
-      "bg-white mx-auto mb-24 rounded-3xl w-[1100px] max-w-[90%] h-[3000px]",
+      "bg-white mx-auto mb-24 rounded-3xl w-[1100px] max-w-[90%]",
       className
     ])}>
       {children}
