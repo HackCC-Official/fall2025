47c4e15fd94668d4c4f2880d479e00ed0cb87047
chheangg <balldremz@gmail.com>
2025-03-20 16:20:40 -0700

    add basic application ui

---
+++ client/src/features/application/types/status.enum.ts
@@ -0,0 +1,5 @@
+export enum ApplicationStatus {
+  SUBMITTED = 'SUBMITTED',
+  ACCEPTED = 'ACCEPTED',
+  DENIED = 'DENIED'
+}
--- client/src/pages/panel/event/index.tsx
+++ client/src/pages/panel/event/index.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { useRouter } from "next/router";
 import { useQuery } from "@tanstack/react-query";
+import { PanelHeader } from "@/components/panel-header";
 export default function EventPage() {
   const router = useRouter()
   const { isLoading, data } = useQuery({
@@ -13,8 +14,8 @@ export default function EventPage() {
   })
 
   return (
-    <div className="py-10">
-      <h1 className="font-bold text-3xl">Event</h1>
+    <div>
+      <PanelHeader>Events</PanelHeader>
       <div className="flex space-x-4 my-4">
         <Button onClick={() => router.push('/panel/event/create')}>
           <Plus />
