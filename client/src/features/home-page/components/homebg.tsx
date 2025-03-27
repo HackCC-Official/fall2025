12165f3b6cb687f5402d1db6a587e94c06d37ee8
chheangg <balldremz@gmail.com>
2025-03-26 19:37:22 -0700

    change img

--- client/src/features/home-page/components/homebg.tsx
+++ client/src/features/home-page/components/homebg.tsx
@@ -7,6 +7,7 @@ import CloudBack from "../../../../public/Violet Cloud Cluster Opaque 1.webp"
 import CloudMid from "../../../../public/Violet Cloud Cluster 2.webp"
 import CloudFront from "../../../../public/Purple Cloud Cluster 1.webp"
 import Moon from "../../../../public/Moon.webp"
+import CatCloud from '../../../../public/Cat Cloud.webp'
 import { Sky } from '@/components/sky';
 
 export const Homebg = () => {
@@ -14,7 +15,7 @@ export const Homebg = () => {
         <div className='z-10'>
             
             <Sky></Sky>
-            <Image priority loading='eager' width={700} height={700} src="https://minio.hackcc.net/public-bucket/Cat_Cloud_1_7.webp" className="-right-[300px] md:-right-[600px] 2xl:-right-[750px] -bottom-8 md:-bottom-40 2xl:-bottom-56 absolute md:w-[1500px] 2xl:w-[2000px] min-w-[700px] md:min-w-[1500px] 2xl:min-w-[2000px] h-auto animate-swaying ease-linear" alt="Cat Cloud Cluster" sizes="(min-width: 1540px) 2000px, (min-width: 780px) 1500px, 700px" ></Image>
+            <Image priority loading='eager' width={700} height={700} src={CatCloud} className="-right-[300px] md:-right-[600px] 2xl:-right-[750px] -bottom-8 md:-bottom-40 2xl:-bottom-56 absolute md:w-[1500px] 2xl:w-[2000px] min-w-[700px] md:min-w-[1500px] 2xl:min-w-[2000px] h-auto animate-swaying ease-linear" alt="Cat Cloud Cluster" sizes="(min-width: 1540px) 2000px, (min-width: 780px) 1500px, 700px" ></Image>
             <Image className="top-0 md:top-12 2xl:top-10 right-0 md:right-8 2xl:right-20 absolute w-auto h-40 sm:h-44 md:h-50 2xl:h-56"  src={Moon} alt="Moon" sizes="(min-width: 1540px) 274px, (min-width: 640px) 215px, 196px" ></Image>
             <div className="bottom-[45%] -left-60 md:-left-48 2xl:-left-32 absolute 2xl:[&>*]:min-w-[1100px]md:[&>*]:w-[800px] 2xl:[&>*]:w-[1100px] [&>*]:w-[500px] md:[&>*]:min-w-[800px] [&>*]:min-w-[500px] [&>*]:h-auto">
                 <Image src={CloudBack} alt="Back CLoud" sizes="(min-width: 1540px) 1100px, (min-width: 780px) 800px, 500px" ></Image>
