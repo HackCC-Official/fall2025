1282a7fa1efaa9f5803abaad28944a618e538660
Gabriel Flores <cybergabriel36@gmail.com>
2025-03-31 21:22:47 -0700

    updated event cards

--- client/src/features/events/ui/plane.tsx
+++ client/src/features/events/ui/plane.tsx
@@ -8,7 +8,7 @@ import Image from "next/image"
              <div className="z-10 relative flex-shrink-0 mr-10 w-[300px] lg:w-[400px] h-auto overflow-hidden">
                 <Image src={spritesheet} className="min-w-[1500px] lg:min-w-[2000px] h-auto animate-moveSpriteSheet" alt="plane"></Image>
             </div>
-             <Image src={ropeimg} alt="rope" className="invisible md:visible md:bottom-[90px] lg:bottom-[140px] md:left-[110px] lg:left-[210px] z-0 absolute w-[250px] h-auto"></Image>
+             <Image src={ropeimg} alt="rope" className="invisible md:visible md:bottom-[90px] lg:bottom-[140px] md:left-[110px] lg:left-[210px] -z-10 absolute w-[250px] h-auto"></Image>
          </div>
      )
  }
