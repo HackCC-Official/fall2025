79942c8d340400df6ca1bec8349053d53125b498
Gabriel Flores <cybergabriel36@gmail.com>
2025-03-31 21:39:51 -0700

    oops

--- client/src/features/events/ui/Card.tsx
+++ client/src/features/events/ui/Card.tsx
@@ -9,7 +9,7 @@ export default function Card({day="day -1", text="text goes here"}:props) {
         <div className="flex-shrink-0 bg-white hover:shadow-xl px-2 py-5 md:py-3 rounded-lg w-[90%] max-w-sm h-[250px]">
             <h2 className="font-bagel text-hoverpurple text-xl">{day}</h2>
             <h3 className="font-mont font-extrabold">what to expect</h3>
-            <p className="font-mont text-graytext text-xs md:text-base 2xl:text-lg">{text}</p>
+            <p className="font-mont text-graytext text-xs md:text-base">{text}</p>
         </div>
     )
 }
