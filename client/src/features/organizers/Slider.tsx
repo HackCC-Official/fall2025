a533023afbc169dc5ba3d19ec44b7702bf1c2fa6
Gabriel Flores <cybergabriel36@gmail.com>
2025-03-31 20:04:26 -0700

    updated organizer cards with newer members

--- client/src/features/organizers/Slider.tsx
+++ client/src/features/organizers/Slider.tsx
@@ -17,6 +17,8 @@ import danish from "../../../public/danish.webp"
 import brian from "../../../public/brian.webp"
 import nataly from "../../../public/nataly.webp"
 import nihal from "../../../public/nihal.webp"
+import jack from "../../../public/jack.webp"
+import danny from "../../../public/danny.webp"
 import peng from "../../../public/peng.webp"
 import tiffany from "../../../public/tiffany.webp"
 import ken from "../../../public/ken.webp"
@@ -26,7 +28,9 @@ import aiden from "../../../public/aiden.webp"
 import anirudh from "../../../public/anirudh.webp"
 import jennifer from "../../../public/jennifer.webp"
 import melody from "../../../public/melody.webp"
+import isabela from "../../../public/isabela.webp"
 import lexington from "../../../public/lexington.webp"
+import evan from "../../../public/evan.webp"
 import christian from "../../../public/chris.webp"
 import gabriel from "../../../public/gabriel.webp"
 import remiel from "../../../public/remiel.webp"
@@ -56,13 +60,16 @@ export default function Slider () {
                             <div className="embla__slide"><Slide indvidualName1='Yasir White' indvidualRole1='Lead Organizer' indviduaImageVaraible1={yasir} indvidualName2='Kairi Navratil' indvidualRole2='Operations Lead' indviduaImageVaraible2={kairi}></Slide></div>
                             <div className="embla__slide"><Slide indvidualName1='Danish Saini' indvidualRole1='Logistics Lead' indviduaImageVaraible1={danish} indvidualName2='Brian Sawaya' indvidualRole2='Logistics' indviduaImageVaraible2={brian}></Slide></div>
                             <div className="embla__slide"><Slide indvidualName1='Nataly Castillo' indvidualRole1='Logistics' indviduaImageVaraible1={nataly} indvidualName2='Nihal' indvidualRole2='Logistics' indviduaImageVaraible2={nihal}></Slide></div>
-
+                            <div className="embla__slide"><Slide indvidualName1='Jack Hu' indvidualRole1='Logistics' indviduaImageVaraible1={jack} indvidualName2='Danny Abraham' indvidualRole2='Logistics' indviduaImageVaraible2={danny}></Slide></div>
                             <div className="embla__slide"><Slide indvidualName1='Peng Tang' indvidualRole1='Logistics' indviduaImageVaraible1={peng} indvidualName2='Tiffany Nguyen' indvidualRole2='Outreach Lead' indviduaImageVaraible2={tiffany}></Slide></div>
 
                             <div className="embla__slide"><Slide indvidualName1='Ken Phongpharnich' indvidualRole1='Outreach' indviduaImageVaraible1={ken} indvidualName2='Sonny' indvidualRole2='Outreach' indviduaImageVaraible2={sonny}></Slide></div>
                             <div className="embla__slide"><Slide indvidualName1='Bhavanbir Dhandi' indvidualRole1='Social Media Lead' indviduaImageVaraible1={bhavanbir} indvidualName2='Aiden Solomon' indvidualRole2='Social Media' indviduaImageVaraible2={aiden}></Slide></div>
                             <div className="embla__slide"><Slide indvidualName1='Anirudh' indvidualRole1='Social Media' indviduaImageVaraible1={anirudh} indvidualName2='Jennifer' indvidualRole2='Social Media' indviduaImageVaraible2={jennifer}></Slide></div>
-                            <div className="embla__slide"><Slide indvidualName1='Melody' indvidualRole1='Social Media' indviduaImageVaraible1={melody} indvidualName2='Lexington Carey' indvidualRole2='Website Lead' indviduaImageVaraible2={lexington}></Slide></div>
+
+                            <div className="embla__slide"><Slide indvidualName1='Melody' indvidualRole1='Social Media' indviduaImageVaraible1={melody} indvidualName2='Isabela Arca' indvidualRole2='Social Media' indviduaImageVaraible2={isabela}></Slide></div>
+                            <div className="embla__slide"><Slide indvidualName1='Lexington Carey' indvidualRole1='Website Lead' indviduaImageVaraible1={lexington} indvidualName2='Evan Ly' indvidualRole2='Website' indviduaImageVaraible2={evan}></Slide></div>
+
                             <div className="embla__slide"><Slide indvidualName1='Christian Figueroa' indvidualRole1='Website' indviduaImageVaraible1={christian} indvidualName2='Gabriel Flores' indvidualRole2='Website' indviduaImageVaraible2={gabriel}></Slide></div>
                             <div className="embla__slide"><Slide indvidualName1='Remiel Shirazi' indvidualRole1='Website' indviduaImageVaraible1={remiel} indvidualName2='Kiwi' indvidualRole2='Mascot' indviduaImageVaraible2={kiwi}></Slide></div>
                         </div>
