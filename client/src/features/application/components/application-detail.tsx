b8de300025a87305ba748c1cd0283430939897a6
chheangg <balldremz@gmail.com>
2025-04-16 14:58:02 -0700

    make react-pdf component dynamically imported

--- client/src/features/application/components/application-detail.tsx
+++ client/src/features/application/components/application-detail.tsx
@@ -5,7 +5,7 @@ import { CircleUser } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import { EmailBadge, ContactBadge, SchoolBadge } from "./application-status";
 import { Separator } from "@/components/ui/separator";
-import { Document, Page, pdfjs } from 'react-pdf'
+import { pdfjs } from 'react-pdf'
 import { useMemo, useState } from "react";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
@@ -18,6 +18,7 @@ import { SubmissionRequestDTO } from "@/features/submission/types/submission-req
 import { useQuery } from "@tanstack/react-query";
 import { Skeleton } from "@/components/ui/skeleton";
 import { getSubmissionById } from "@/features/submission/api/submission";
+import dynamic from "next/dynamic";
 
 export function ApplicationHeader({ application } : { application: ApplicationResponseDTO }) {
   return (
@@ -74,6 +75,16 @@ export function ApplicationResponse({ response } : { response: SubmissionRequest
     </Card>
   )
 }
+
+// Dynamically import react-pdf with no SSR
+const Document = dynamic(
+  () => import('react-pdf').then((mod) => mod.Document),
+  { ssr: false }
+);
+const Page = dynamic(
+  () => import('react-pdf').then((mod) => mod.Page),
+  { ssr: false }
+);
 
 export function ApplicationFile({ prompt, url }: { prompt: string, url: string }) {
   const [numPages, setNumPages] = useState(0);
