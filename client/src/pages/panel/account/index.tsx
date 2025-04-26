bc61c362a14ddc1f4237ef910d3b114205326075
chheangg <balldremz@gmail.com>
2025-04-26 00:42:50 -0700

    add context menu for user and user edit feature

--- client/src/pages/panel/account/index.tsx
+++ client/src/pages/panel/account/index.tsx
@@ -3,19 +3,38 @@ import PanelLayout from "../layout"
 import { Plus } from "lucide-react"
 import { DataTable } from "@/components/data-table";
 import { columns } from "@/features/account/components/account-table/columns";
-import { useQuery } from "@tanstack/react-query";
-import { getAccounts } from "@/features/account/api/account";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { deleteAccount, getAccounts } from "@/features/account/api/account";
 import { AccountForm } from "@/features/account/components/account-form";
 import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 import { Separator } from "@/components/ui/separator";
 import { useState } from "react";
+import { AccountDTO } from "@/features/account/types/account-dto";
 
 export default function AccountPage() {
+  const [accountId, setAccountId] = useState<string>('');
+  const queryClient = useQueryClient();
   const [open, setOpen] = useState(false)
   const { isLoading, data } = useQuery({
     queryKey: ['accounts'],
     queryFn: () => getAccounts()
   })
+
+  const deleteAccountMutation = useMutation({
+    mutationFn: async (accountId: string) => deleteAccount({ id: accountId }),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['accounts'] })
+  });
+
+  function onEdit(value: AccountDTO) {
+    setAccountId(value.id);
+    setTimeout(() => {
+      setOpen(true);
+    }, 0);
+  }
+
+  function onDelete(value: AccountDTO) {
+    deleteAccountMutation.mutate(value.id)
+  }
   return (
     <div>
       <h1 className="font-bold text-3xl">Account</h1>
@@ -35,11 +54,18 @@ export default function AccountPage() {
                 </SheetDescription>
               </SheetHeader>
               <Separator className="my-4" />
-              <AccountForm setOpen={setOpen} />
+              <AccountForm accountId={accountId} setOpen={setOpen} />
           </SheetContent>
         </Sheet>
       </div>
-      <DataTable isLoading={isLoading} data={data || []} columns={columns} />
+      <DataTable 
+        isLoading={isLoading} 
+        data={data || []} 
+        columns={columns} 
+        onEdit={onEdit}
+        onDelete={onDelete}
+        enableRightClick 
+      />
     </div>
   )
 }
