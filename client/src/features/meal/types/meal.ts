80a65bb7e630693329511f0007760753c339ac47
chheangg <balldremz@gmail.com>
2025-04-29 15:23:26 -0700

    create types for meal

---
+++ client/src/features/meal/types/meal.ts
@@ -0,0 +1,21 @@
+import { AccountDTO } from "@/features/account/types/account-dto";
+
+export enum MealType {
+  BREAKFAST = 'BREAKFAST',
+  LUNCH = 'LUNCH',
+  DINNER = 'DINNER',
+  UNCLAIMED = 'UNCLAIMED'
+}
+
+export interface ResponseMealAccountDTO {
+  id: string;
+  account: AccountDTO;
+  event_id: string;
+  mealType: MealType ;
+  checkedInAt: string;
+}
+
+export interface RequestMealDTO {
+  account_id: string;
+  event_id: string;
+}
