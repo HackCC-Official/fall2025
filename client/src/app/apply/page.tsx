945869c081e9eacf9b36a0c4d972e4347b9bc37a
chheangg <balldremz@gmail.com>
2025-08-07 14:47:36 -0700

    feat!: turn application logic into hooks

--- client/src/app/apply/page.tsx
+++ client/src/app/apply/page.tsx
@@ -1,114 +1,40 @@
  
 "use client";
-import { useForm } from "react-hook-form";
 import { Logo } from "@/components/logo";
-import { SkyFixed } from "@/components/sky";
 import { ApplicationLabel } from "@/features/application/components/application-input";
 import { FormCard } from "@/features/application/components/form-card";
-import { useMutation, useQueries, useQueryClient } from "@tanstack/react-query";
-import { QuestionResponseDto } from "@/features/question/types/question-response.dto";
 import { QuestionType } from "@/features/question/types/question-type.enum";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { ApplicationRequestDTO } from "@/features/application/types/application";
 import { ApplicationStatus } from "@/features/application/types/status.enum";
-import { Document } from "@/features/application/api/application";
 import { DarkCard } from "@/components/dark-card";
-import { debounce } from "lodash";
 import { Spinner } from "@/components/ui/spinner";
 import { BackButton } from "@/features/application/components/back-btn";
-import { FormValues, Question, QuestionGroupNode, QuestionNodes } from "@/features/application/components/question-node";
-import { getHackathonQuestions } from "@/features/question/api/hackathon-question";;
-import { getHackathonApplicationByUserId, createHackathonApplication } from "@/features/application/api/hackathon-application";
+import { Question } from "@/features/application/components/question-node";
 import { FrontPageSecondaryLayout } from "@/layouts/front-page-layout";
 import { useAuthentication } from "@/features/auth/hooks/use-authentication";
+import { useApplication } from "@/features/application/hook/use-application";
+import { createHackathonApplication, getHackathonApplicationByUserId } from "@/features/application/api/hackathon-application";
+import { getHackathonQuestions } from "@/features/question/api/hackathon-question";
 
 export default function ApplyPage() {
-    const maxWordLength = 150;
-    const maxCharLength = 900;
-    const maxFileSize = 1000000 * 25
-
-    const formRules = {
-        required: "your response is required for this field.", // Basic required validation
-        validate: {
-            maxFileSize: (value: unknown) => {
-                // Handle single file case
-                if (value instanceof File) {
-                    return value.size > maxFileSize 
-                    ? "File size must be under 25 MB."
-                    : true;
-                }
-                
-                if (Array.isArray(value)) {
-                    for (const file of value) {
-                        if (file instanceof File && file.size > maxFileSize) {
-                        return "File size must be under 25 MB.";
-                        }
-                    }
-                }
-                
-                // Handle case where value might be a FileList
-                if (value instanceof FileList) {
-                    for (let i = 0; i < value.length; i++) {
-                        if (value[i].size > maxFileSize) {
-                        return "File size must be under 25 MB.";
-                        }
-                    }
-                }
-                
-                return true;
-            },
-            maxLength: (value: File | string | number | null) => {
-                if (typeof value === 'string')  {
-                    if (debouncedLargerThanMaxWordLength(value)) {
-                        return `only a maximum of ${maxWordLength} words or ${maxCharLength} characters are allowed.`
-                    }
-                }
-                return true;
-            }
-        }
-    }
-
-    const queryClient = useQueryClient();
     const { user, authCheck } = useAuthentication();
-
-    const [applicationQuery, questionQuery] = useQueries({
-        queries: [
-            {
-                queryKey: ['hackathon-application-user', user ? user.id : ''],
-                queryFn: () => getHackathonApplicationByUserId(user ? user.id : ''),
-                enabled: () => !!(user && user.id)
-            },
-            {
-                queryKey: ['hackathon-questions'],
-                queryFn: () => getHackathonQuestions()
-            }
-        ]
+    const { 
+        formRules,
+        applicationQuery, 
+        questionQuery, 
+        applicationMutation, 
+        nodes, 
+        handleSubmit, 
+        onSubmit,
+        control 
+    } = useApplication({
+        key: 'hackathon',
+        user,
+        getApplicationByUserId: getHackathonApplicationByUserId,
+        getQuestions: getHackathonQuestions,
+        createApplication: createHackathonApplication
     })
-
-    const applicationMutation = useMutation({
-        mutationFn: (
-            { 
-            applicationDTO, 
-            document 
-            } 
-            : 
-            { 
-                applicationDTO: ApplicationRequestDTO, 
-                document: Document 
-            }
-            ) => 
-                createHackathonApplication(applicationDTO, document)
-        ,
-        onSettled: () => {
-            queryClient.invalidateQueries({ queryKey: ['application-user', user ? user.id : ''] })
-        }
-    })
-
-    const { control, handleSubmit } = useForm<FormValues>({
-        mode: 'onChange', // This enables validation on change
-        reValidateMode: 'onChange', // This ensures re-validation happens on change
-    });
 
     if (!authCheck || applicationQuery.isLoading || questionQuery.isLoading) {
         return (
@@ -139,99 +65,6 @@ export default function ApplyPage() {
             </FrontPageSecondaryLayout>
         )
     }
-
-    const nodes: QuestionNodes[] = questionQuery.data ? questionQuery.data.reduce<QuestionNodes[]>((prev, curr) => {
-        if (curr.group) {
-            const groupNodeIndex = prev.findIndex(n => n.type === 'GROUP' && n.label === curr.group);
-            if (groupNodeIndex === -1) {
-                prev.push({
-                    type: "GROUP",
-                    isSingleLabel: curr.isSingleLabel,
-                    label: curr.group,
-                    questions: [curr]
-                });
-            } else {
-                (prev[groupNodeIndex] as QuestionGroupNode).questions.push(curr);
-            }
-        } else {
-            prev.push({
-                type: 'NODE',
-                question: curr
-            });
-        }
-        return prev;
-    }, []) : [];
-
-    const onSubmit = async (values: FormValues) => {
-        const document: Document = {
-            resume: undefined,
-            transcript: undefined
-        }
-        const application: ApplicationRequestDTO = {
-            userId: user?.id || '',
-            status: ApplicationStatus.SUBMITTED,
-            firstName: "",
-            lastName: "",
-            email: "",
-            phoneNumber: "",
-            school: "",
-            submissions: [],
-        };
-        if (values && questionQuery.data) {
-            Object.entries(values).forEach(([key, value]) => {
-                const question = questionQuery.data.find(q => q.id === Number(key.split('_')[1])) as QuestionResponseDto;
-                if (question?.isApplicationField) {
-                    // Assign the value to the application field
-                    switch (question.applicationField) {
-                        case "firstName":
-                        case "lastName":
-                        case "email":
-                        case "phoneNumber":
-                        case "school":
-                            application[question.applicationField] = String(value);
-                            break;
-                        case "status":
-                            application.status = ApplicationStatus.SUBMITTED; // Default status
-                            break;
-                        case "transcript":
-                            document.transcript = (value as unknown as File[])[0]
-                            break;
-                        case "resume":
-                            document.resume = (value as unknown as File[])[0];
-                            break;
-                        default:
-                            break;
-                    }
-                } else {
-                    application.submissions.push({
-                        questionId: Number(key.split('_')[1]),
-                        answer: String(value)
-                    });
-                }
-            });
-
-            // Submit to your API here
-            await applicationMutation.mutateAsync({ 
-                applicationDTO: application, 
-                document
-            })
-        }
-    };
-
-    function largerThanMaxWordLength(text: string) {
-        if (text.length > maxCharLength) {
-            return true;
-        }
-    
-        const len = text.split(/[\s]+/);
-        if(len.length > maxWordLength){
-            return true;
-        }
-
-        return false;
-    }
-
-    const debouncedLargerThanMaxWordLength = debounce((value: string) => largerThanMaxWordLength(value), 500)
 
     return (
         <FrontPageSecondaryLayout>
