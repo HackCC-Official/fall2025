37722fe257a94112c748ef8ca0bcec249b184d2c
chheangg <balldremz@gmail.com>
2025-10-30 00:35:54 -0700

    fix: attempt fix on bg

--- client/src/app/attendee-dashboard/page.tsx
+++ client/src/app/attendee-dashboard/page.tsx
@@ -1,62 +1,7 @@
-"use client"; 
-
-import React, { useState, useEffect } from 'react';
-
-
-import {
-    Badge,
-    CalendarDays,
-    Timer,
-    Bell,
-    ScrollText,
-    Users,
-    Download,
-    Menu,
-    X
-} from 'lucide-react';
-
-import { MainContent } from '@/features/attendee-dashboard/components/main-content';
-import { Sidebar } from '@/features/attendee-dashboard/components/sidebar';
+import { AttendeeDashboard } from "@/features/attendee-dashboard/components/attendee-dashboard";
 
 export default function AttendeeDashboardPage() {
-    const [activeSection, setActiveSection] = useState('hackpass');
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
     return (
-        <div 
-            className="flex md:flex-row flex-col min-h-screen"
-            style={{
-                backgroundImage: 'url("/Hero%20Background.webp")', 
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundAttachment: 'fixed'
-            }}
-        >
-            {/* Mobile Header */}
-            <header className="md:hidden flex justify-between items-center bg-[#4A376B] p-4 border-[#523B75] border-b w-full">
-                <div className="flex items-center gap-2">
-                    <div className="flex justify-center items-center w-8 h-8">
-                        {/* <Logo className="w-full h-full object-contain" /> */}
-                    </div>
-                    <h2 className="font-mont font-bold text-white text-xl">HackCC</h2>
-                </div>
-                <button
-                    onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-                    className="p-2 text-gray-300 hover:text-white"
-                >
-                    {isSidebarOpen ? <X size={24} strokeWidth={2.5} /> : <Menu size={24} strokeWidth={2.5} />}
-                </button>
-            </header>
-
-            <Sidebar
-                activeSection={activeSection}
-                setActiveSection={(section) => {
-                    setActiveSection(section);
-                    setIsSidebarOpen(false); // Close sidebar on mobile nav
-                }}
-                isOpen={isSidebarOpen}
-            />
-            <MainContent activeSection={activeSection} />
-        </div>
-    );
+        <AttendeeDashboard />
+    )
 }
