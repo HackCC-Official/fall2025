23adf09d4cb5b737d8ba8d9b5b28586391a31208
chheangg <balldremz@gmail.com>
2025-11-08 10:33:54 -0800

    fix: stuff

--- client/src/features/attendee-dashboard/utils/deadline.tsx
+++ client/src/features/attendee-dashboard/utils/deadline.tsx
@@ -2,11 +2,9 @@
 
 import { Countdown } from "../types/attendee-dashboard";
 
-const HACKATHON_DEADLINE = new Date('2025-11-09T12:00:00').getTime();
-
-export const calculateTimeLeft = (HACKATHON_START_TIME: number): Countdown | null => {
+export const calculateTimeLeft = (targetTime: number): Countdown | null => {
     const now = new Date().getTime();
-    const difference = HACKATHON_DEADLINE - now;
+    const difference = targetTime - now;
 
     if (difference <= 0) {
         return null; // Deadline has passed
